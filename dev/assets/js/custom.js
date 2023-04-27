acec4a6ea9c6d4f09624b55f5f7cd2165fa65cdc
anna-okhrimenko <125506103+anna-okhrimenko@users.noreply.github.com>
2023-04-27 22:42:11 +0300

    form select

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -4,4 +4,39 @@ $(document).ready(function () {
         arrows: false,
         speed: 800,
     });
+
+});
+
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const arrow = dropdown.querySelector('.arrow');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        arrow.classList.toggle('arrow-rotate');
+        menu.classList.toggle('menu-open');
+    })
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerHTML = option.innerHTML;
+            select.classList.remove('select-clicked');
+            arrow.classList.remove('arrow-rotate');
+            menu.classList.remove('menu-open');
+                        
+        });
+    });
+
+    document.addEventListener('click', (e) => {
+        const clickDropdown = e.composedPath().includes(dropdown);
+        if(!clickDropdown) {
+            select.classList.remove('select-clicked');
+            arrow.classList.remove('arrow-rotate');
+            menu.classList.remove('menu-open');
+        }
+    });
 });
